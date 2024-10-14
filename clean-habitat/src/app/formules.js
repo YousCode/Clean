9aebb5af4f64e4b178edbf6b77081f92f2eab5b6
Youssouph Camara <88164361+YousCode@users.noreply.github.com>
2024-10-14 16:30:08 +0200

    add push

--- clean-habitat/src/app/formules.js
+++ clean-habitat/src/app/formules.js
@@ -62,7 +62,7 @@ Porte claquée: Intervention d'urgence pour «porte claquée».`,
 
   return (
     <div className="container mx-auto px-4 py-20">
-      <h1 className="text-4xl font-bold mb-8 text-center">Découvrez Nos Formules</h1>
+
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {formules.map((formule) => (
           <div
