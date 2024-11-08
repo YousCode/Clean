743b09771f62a393ad6ac189959e5ce859d5a93f
Youssouph Camara <88164361+YousCode@users.noreply.github.com>
2024-11-08 14:02:24 +0100

    add fav

--- clean-habitat/src/app/layout.js
+++ clean-habitat/src/app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
         <meta property="og:url" content="https://cleanhabitatsolutions.com" />
         <meta property="og:type" content="website" />
         
-        {/* Lien vers le favicon */}
+        {/* Favicon */}
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
